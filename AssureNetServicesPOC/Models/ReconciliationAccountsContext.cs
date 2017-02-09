04a1a4a73492771d3b81a704e5308b741dd6a056
vechamkarunakar <kavecham@microsoft.com>
2017-02-09 09:35:29 -0800

    Add project files.

---
+++ AssureNetServicesPOC/Models/ReconciliationAccountsContext.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.Entity;
+namespace AssureNetServicesPOC.Models
+{
+    public class ReconciliationAccountsContext : DbContext
+    {
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            Database.SetInitializer<ReconciliationAccountsContext>(null);
+
+            modelBuilder.Entity<ReconAccount>().ToTable("ReconAccount");
+            modelBuilder.Entity<ReconAccount>().HasKey(t => new { t.Id});
+        }
+        public ReconciliationAccountsContext()
+                : base("name=AssureNetContext")
+        {
+        }
+        public DbSet<ReconAccount> ReconAccounts { get; set; }
+
+
+    }
+}
